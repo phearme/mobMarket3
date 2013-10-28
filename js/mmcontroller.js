c19e97ae4479d0e726aaa811281e6c6a7b144976
R. Cohen <raphael.cohen@gmail.com>
2013-10-29 00:11:15 +0100

    fix touch events

--- js/mmcontroller.js
+++ js/mmcontroller.js
@@ -27,57 +27,6 @@ document.addEventListener("deviceready", function () {
 			window.open(link, "_system");
 		};
 
-		// new data callback
-		$scope.onGetNewsData = function (data) {
-			var itemNodes = data.getElementsByTagName("item"), items = [],
-				i, j, item, titleNode, linkNode, pubDate, description,
-				parsedDesc, descImages, descFonts, img, fontTagOccurance;
-			$scope.$apply($scope.loading = false);
-			if (itemNodes && itemNodes.length > 0) {
-				for (i = 0; i < itemNodes.length; i += 1) {
-					item = {};
-					titleNode = itemNodes[i].getElementsByTagName("title");
-					if (titleNode && titleNode.length > 0) { item.title = titleNode[0].textContent; }
-					linkNode = itemNodes[i].getElementsByTagName("link");
-					if (linkNode && linkNode.length > 0) { item.link = linkNode[0].textContent; }
-					pubDate = itemNodes[i].getElementsByTagName("pubDate");
-					if (pubDate && pubDate.length > 0) { item.pubDate = pubDate[0].textContent; }
-					description = itemNodes[i].getElementsByTagName("description");
-					if (description && description.length > 0) {
-						parsedDesc = (new window.DOMParser()).parseFromString(description[0].textContent, "text/xml");
-						descImages = parsedDesc.getElementsByTagName("img");
-						if (descImages && descImages.length > 0) {
-							for (j = 0; j < descImages.length; j += 1) {
-								if (descImages[j].getAttribute("src") && descImages[j].getAttribute("src") !== "") {
-									item.image = descImages[j].getAttribute("src");
-									break;
-								}
-							}
-						}
-						descFonts = parsedDesc.getElementsByTagName("font");
-						if (descFonts && descFonts.length > 0) {
-							fontTagOccurance = 0;
-							for (j = 0; j < descFonts.length; j += 1) {
-								if (descFonts[j].getAttribute("size") === "-1") {
-									fontTagOccurance += 1;
-									if (fontTagOccurance === 2) {
-										if (descFonts[j].textContent.length > 0) {
-											item.content = descFonts[j].textContent;
-										}
-										break;
-									}
-								}
-							}
-						}
-					}
-					items.push(item);
-				}
-			}
-			if (items.length > 0) {
-				$scope.$apply($scope.newsItems = items);
-			}
-		};
-
 		// simple screen navigation helpers
 		$scope.selectScreen = function (s) {
 			if (!s) {
@@ -92,7 +41,12 @@ document.addEventListener("deviceready", function () {
 			switch ($scope.selectedScreen.id) {
 			case "news":
 				$scope.loading = true;
-				newsReader.getNews(undefined, $scope.onGetNewsData);
+				newsReader.getNews(undefined, function (items) {
+					$scope.$apply($scope.loading = false);
+					if (items && items.length > 0) {
+						$scope.$apply($scope.newsItems = items);
+					}
+				});
 				break;
 			}
 		};
@@ -115,26 +69,32 @@ document.addEventListener("deviceready", function () {
 	// touch directive
 	.directive("touchBtn", function () {
 		"use strict";
-		function restoreDefault(e) {
-			e.style.opacity = "1";
-			e.removeEventListener("touchstart");
-			e.removeEventListener("touchmove");
-			e.removeEventListener("touchend");
-		}
-
 		return function (scope, element, attrs) {
-			var elm = element[0];
-			elm.removeEventListener("touchstart");
-			elm.addEventListener("touchstart", function () {
+			var elm = element[0],
+				touchstartEvent,
+				touchmoveEvent,
+				touchendEvent,
+				restoreDefault = function () {
+					elm.style.opacity = "1";
+					elm.removeEventListener("touchstart");
+					elm.removeEventListener("touchmove");
+					elm.removeEventListener("touchend");
+				};
+			touchmoveEvent = function () {
+				restoreDefault();
+			};
+			touchendEvent = function () {
+				if (attrs.touchAction && scope[attrs.touchAction] && typeof scope[attrs.touchAction] === "function") {
+					scope.$apply(scope[attrs.touchAction](attrs.touchBtn));
+				}
+				restoreDefault();
+			};
+			touchstartEvent = function () {
 				elm.style.opacity = "0.6";
-				elm.addEventListener("touchend", function () {
-					if (attrs.touchAction && scope[attrs.touchAction] && typeof scope[attrs.touchAction] === "function") {
-						scope.$apply(scope[attrs.touchAction](attrs.touchBtn));
-					}
-					restoreDefault(elm);
-				});
-				elm.addEventListener("touchmove", function () {	restoreDefault(elm); });
-			});
+				elm.addEventListener("touchend", touchendEvent);
+				elm.addEventListener("touchmove", touchmoveEvent);
+			};
+			elm.addEventListener("touchstart", touchstartEvent);
 		};
 	});
 /*
--- js/newsreader.js
+++ js/newsreader.js
@@ -13,9 +13,56 @@ var NewsReader = (function () {
 		}
 		xhr.open("GET", url, true);
 		xhr.onreadystatechange = function () {
+			var data, itemNodes, items = [], i, j,
+				item, titleNode, linkNode, pubDate, description,
+				parsedDesc, descImages, descFonts, fontTagOccurance;
 			if (xhr.readyState === 4) {
 				if (xhr.status === 200) {
-					callback((new window.DOMParser()).parseFromString(xhr.responseText, "text/xml"));
+					data = (new window.DOMParser()).parseFromString(xhr.responseText, "text/xml");
+					itemNodes = data.getElementsByTagName("item");
+					if (itemNodes && itemNodes.length > 0) {
+						for (i = 0; i < itemNodes.length; i += 1) {
+							item = {};
+							titleNode = itemNodes[i].getElementsByTagName("title");
+							if (titleNode && titleNode.length > 0) { item.title = titleNode[0].textContent; }
+							linkNode = itemNodes[i].getElementsByTagName("link");
+							if (linkNode && linkNode.length > 0) { item.link = linkNode[0].textContent; }
+							pubDate = itemNodes[i].getElementsByTagName("pubDate");
+							if (pubDate && pubDate.length > 0) { item.pubDate = pubDate[0].textContent; }
+							description = itemNodes[i].getElementsByTagName("description");
+							if (description && description.length > 0) {
+								parsedDesc = (new window.DOMParser()).parseFromString(description[0].textContent, "text/xml");
+								descImages = parsedDesc.getElementsByTagName("img");
+								if (descImages && descImages.length > 0) {
+									for (j = 0; j < descImages.length; j += 1) {
+										if (descImages[j].getAttribute("src") && descImages[j].getAttribute("src") !== "") {
+											item.image = descImages[j].getAttribute("src");
+											break;
+										}
+									}
+								}
+								descFonts = parsedDesc.getElementsByTagName("font");
+								if (descFonts && descFonts.length > 0) {
+									fontTagOccurance = 0;
+									for (j = 0; j < descFonts.length; j += 1) {
+										if (descFonts[j].getAttribute("size") === "-1") {
+											fontTagOccurance += 1;
+											if (fontTagOccurance === 2) {
+												if (descFonts[j].textContent.length > 0) {
+													item.content = descFonts[j].textContent;
+												}
+												break;
+											}
+										}
+									}
+								}
+							}
+							items.push(item);
+						}
+						callback(items);
+					} else {
+						callback(false);
+					}
 				} else {
 					callback(false);
 				}
